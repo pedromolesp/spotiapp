964309a58fcee89fa0f20ffd95c2a3b895e53ac4
pedro.molina <pedro.molina@petroprix.com>
2020-08-12 19:32:59 +0200

    class 110 finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,13 +10,20 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class HomeComponent {
   nuevasCanciones: any[] = [];
+  error: boolean;
+  errorMessage: string;
   loading: boolean;
   constructor(private spotify: SpotifyService) {
     this.loading = true;
+    this.error = false;
     this.spotify.getNewReleases().subscribe(
       (data: any) => {
         this.nuevasCanciones = data;
         this.loading = false;
+      }, (errorServicio) => {
+        this.error = true;
+        this.loading = false;
+        this.errorMessage = errorServicio.error.error.message;
       }
 
     );
